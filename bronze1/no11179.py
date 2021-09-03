0e46d401925fcf9afba6c2f9b99f7f46b8fdb11c
Awos <uws0331@gmail.com>
2021-09-03 21:55:15 +0900

    백준 11179번 2진수 뒤집기 - 2번째 풀이

--- bronze1/no11179.py
+++ bronze1/no11179.py
@@ -1,3 +1,4 @@
-s_N = bin(int(input())).replace("0b", "")
-arr_list = '0b'+''.join(list(reversed(s_N)))
-print(int(arr_list, 2))
+# s_N = bin(int(input())).replace("0b", "")
+# arr_list = '0b'+''.join(list(reversed(s_N)))
+# print(int(arr_list, 2))
+print(int(bin(int(input()))[2:][::-1], 2))
