352a4c21587256124bb2370f2b04d675aef45ae4
Awos <uws0331@gmail.com>
2021-09-07 23:21:52 +0900

    백준 1453번 피시방 알바

---
+++ bronze1/no1453.py
@@ -0,0 +1,10 @@
+N = int(input())
+customer = list(map(int, input().split()))
+cus_list = [0] + [0 for j in range(1, 101)]
+cnt = 0
+for i in customer:
+    if cus_list[i] == 0:
+        cus_list[i] = i
+    else:
+        cnt += 1
+print(cnt)
