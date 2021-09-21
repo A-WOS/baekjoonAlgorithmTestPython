e7088d6db0fdaf359a2a571bc413b46dc5b623a0
Awos <uws0331@gmail.com>
2021-09-21 18:05:00 +0900

    백준 20112번 사토르 마방진 배열명 변경

--- bronze1/no20112.py
+++ bronze1/no20112.py
@@ -2,16 +2,16 @@ import sys
 
 N = int(sys.stdin.readline())
 # 해당 크기 만큼의 배열 초기화
-s1 = [['' for _ in range(N)] for _ in range(N)]
-s2 = [['' for _ in range(N)] for _ in range(N)]
+row_word = [['' for _ in range(N)] for _ in range(N)]
+col_word = [['' for _ in range(N)] for _ in range(N)]
 result = "YES"
 for i in range(N):
     word = input()
     for j, val in enumerate(word, start=0):
-        s1[i][j] = val
-        s2[j][i] = s1[i][j]
+        row_word[i][j] = val
+        col_word[j][i] = row_word[i][j]
 
-if s1 != s2:
+if row_word != col_word:
     result = "NO"
 
 print(result)
