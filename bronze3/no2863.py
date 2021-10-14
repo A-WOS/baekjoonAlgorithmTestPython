ada4f437da5fbdcd902a7bb4eb54987cba2bceed
Awos <uws0331@gmail.com>
2021-10-14 21:18:02 +0900

    백준 2863번 이게 분수?

---
+++ bronze3/no2863.py
@@ -0,0 +1,21 @@
+# swap
+def next(l):
+    temp = l[0][0]
+    l[0][0] = l[1][0]
+    l[1][0] = l[1][1]
+    l[1][1] = l[0][1]
+    l[0][1] = temp
+    return l
+
+
+# 계산
+def cal(l):
+    return l[0][0] / l[1][0] + l[0][1] / l[1][1]
+
+
+l = [list(map(int, input().split())) for x in range(2)]
+result = [cal(l)]
+count = 0
+for i in range(3):
+    result.append(cal(next(l)))
+print(result.index(max(result)))
