b0069758d357caecc7fd2cfc8434a3ef8dcf6596
Awos <uws0331@gmail.com>
2021-10-17 19:11:08 +0900

    백준 11948번 과목선택

---
+++ bronze4/no11948.py
@@ -0,0 +1,10 @@
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+E = int(input())
+F = int(input())
+
+s_l = [A, B, C, D]
+h_l = [E, F]
+print(sum(s_l) - min(s_l) + max(h_l))
