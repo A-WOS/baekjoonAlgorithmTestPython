2f0624b10f3a801dfe74c81190fd1c82351f9250
Awos <uws0331@gmail.com>
2021-11-08 17:09:22 +0900

    백준 17362번 수학은 체육과목 입니다 2

---
+++ bronze4/no17362.py
@@ -0,0 +1,13 @@
+n = int(input())
+rs = 0
+if n % 8 == 1:
+    rs = 1
+elif n% 8 == 5:
+    rs = 5
+elif n % 8 == 0 or n % 8 == 2:
+    rs = 2
+elif n % 8 == 3 or n % 8 == 7:
+    rs = 3
+elif n % 8 == 4 or n % 8 == 6:
+    rs = 4
+print(rs)
