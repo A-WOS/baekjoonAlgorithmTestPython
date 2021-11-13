433bad35f9c12258f89b33f9e99ace1e1e7b5d03
Awos <uws0331@gmail.com>
2021-11-13 16:36:13 +0900

    백준 2720번 세탁소 사장 동혁

---
+++ bronze3/no2720.py
@@ -0,0 +1,15 @@
+from decimal import Decimal
+
+T = int(input())
+q, d, n, p = Decimal('0.25'), Decimal('0.10'), Decimal('0.05'), Decimal('0.01')
+for i in range(T):
+    x = Decimal(input()) * Decimal('0.01')
+    q_c = x // q
+    x %= q
+    d_c = x // d
+    x %= d
+    n_c = x // n
+    x %= n
+    p_c = x // p
+    x %= p
+    print(int(q_c), int(d_c), int(n_c), int(p_c), x)
