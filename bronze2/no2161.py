7c6ca10492c198a65b08068fcf1954efa6d11de0
Awos <uws0331@gmail.com>
2021-11-25 20:53:40 +0900

    백준 2161번 카드1

---
+++ bronze2/no2161.py
@@ -0,0 +1,13 @@
+l = [i for i in range(1, int(input())+1)]
+result = []
+cnt = 1
+while len(l) > 0:
+    if cnt % 2 == 0:
+        l.append(l[0])
+        l.pop(0)
+    else:
+        result.append(l[0])
+        l.pop(0)
+    cnt += 1
+
+print(' '.join(map(str, result)))
