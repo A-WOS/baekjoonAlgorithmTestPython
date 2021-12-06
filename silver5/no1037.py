1e69a9bc965539a03a4e2a2332ce8aa966e4bd43
Awos <uws0331@gmail.com>
2021-12-06 21:53:49 +0900

    백준 1037번 약수 (간결)

--- silver5/no1037.py
+++ silver5/no1037.py
@@ -1,9 +1,3 @@
 N = int(input())
 l = sorted(map(int, input().split()))
-if N % 2 == 1:
-    if N == 1:
-        print(l[0]**2)
-    else:
-        print(l[int(len(l) / 2)]**2)
-else:
-    print(min(l)*max(l))
+print(min(l)*max(l))
