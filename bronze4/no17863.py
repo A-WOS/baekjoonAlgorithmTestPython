f4cfd7e1177ed8665db95694fe40b732471a24cb
Awos <uws0331@gmail.com>
2022-01-06 17:06:43 +0900

    백준 17863번 FYI

---
+++ bronze4/no17863.py
@@ -0,0 +1,11 @@
+print('YES') if input()[0:3] == '555' else print('NO')
+
+# tel = ['555-1212', '555-9876', '555-0000', '555-7777']
+# num = input()
+# num = num[0:3] + '-' + num[3:]
+# flag = False
+# for val in tel:
+#     if num.count(val):
+#         flag = True
+#         break
+# print('YES') if flag else print('NO')
