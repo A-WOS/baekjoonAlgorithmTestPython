bd62afcb45c801603447b1be378a74b5089f600a
Awos <uws0331@gmail.com>
2022-01-12 17:14:14 +0900

    백준 16199번 나이 계산하기

--- bronze4/no16199.py
+++ bronze4/no16199.py
@@ -1,6 +1,18 @@
-# b_y, b_m, b_d = input().split()
-# s_y, s_m, s_d = input().split()
-#
-# man = s_y-b_y+s_m-b_m+s_d-b_d
-# se = s_y-b_y + 1
-# yeon = s_y-b_y
+def man(*args):
+    if b_m == s_m:
+        if b_d <= s_d:
+            return s_y - b_y
+        else:
+            return s_y - b_y - 1
+    elif b_m < s_m:
+        return s_y - b_y
+    else:
+        return s_y - b_y - 1
+
+
+b_y, b_m, b_d = map(int, input().split())
+s_y, s_m, s_d = map(int, input().split())
+
+print(man(b_y, b_m, b_d, s_y, s_m, s_d))
+print(1 + s_y - b_y)
+print(s_y - b_y)
