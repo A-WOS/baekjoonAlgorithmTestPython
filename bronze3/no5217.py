be80d1cc7b9ccac7777c3888dc92aa1242789a4a
Awos <uws0331@gmail.com>
2022-01-14 06:53:11 +0900

    백준 5217번 쌍의 합

--- bronze3/no5217.py
+++ bronze3/no5217.py
@@ -1,5 +1,10 @@
-T = int(input())
-for _ in range(T):
+for _ in range(int(input())):
     x = int(input())
-    # y, z =
-    # print(f'Pairs for {x}:' )
+    n = x//2
+    l = []
+    for i in range(x-1, n, -1):
+        if x - i == i:
+            continue
+        else:
+            l.append(f'{x - i} {i}')
+    print(f'Pairs for {x}: ' + ', '.join(val for val in l).strip())
