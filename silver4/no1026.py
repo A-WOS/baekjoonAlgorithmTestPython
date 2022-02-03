44827c9cdff78c7bac97ce96f919e0ca8e09d77b
Awos <uws0331@gmail.com>
2022-02-03 23:45:09 +0900

    백준 1026번 보물 (조건에 맞게)

--- silver4/no1026.py
+++ silver4/no1026.py
@@ -1,7 +1,17 @@
+# # solved #1
+# n = int(input())
+# l1 = list(reversed(sorted(map(int, input().split()))))
+# l2 = sorted(list(map(int, input().split())))
+# tot = 0
+# for i in range(n):
+#     tot += l1[i] * l2[i]
+# print(tot)
+
+# solved #2
 n = int(input())
-l1 = list(reversed(sorted(map(int, input().split()))))
-l2 = sorted(list(map(int, input().split())))
+l1 = list(map(int, input().split()))
+l2 = list(map(int, input().split()))
 tot = 0
 for i in range(n):
-    tot += l1[i] * l2[i]
+    tot += l1.pop(l1.index(min(l1))) * l2.pop(l2.index(max(l2)))
 print(tot)
