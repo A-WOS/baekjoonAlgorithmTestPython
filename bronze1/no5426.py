74182366d96b102a79c68014ae8dde02dbce733d
Awos <uws0331@gmail.com>
2022-02-09 14:04:08 +0900

    백준 5426번 비밀편지

---
+++ bronze1/no5426.py
@@ -0,0 +1,15 @@
+for _ in range(int(input())):
+    s = list(input())
+    rect = int(len(s) ** 0.5)
+    secrets_l = list()
+    j = 0
+    rs = ''
+    for i in range(rect):
+        secrets_l.append(s[j:j + rect])
+        j += rect
+
+    for i in range(rect-1, -1, -1):
+        for j in range(0, rect):
+            rs += secrets_l[j][i]
+
+    print(rs)
