6fbdf3b077fa16b4b2b4000492cb3dd2a837ef07
Awos <uws0331@gmail.com>
2022-02-12 23:58:21 +0900

    백준 1551번 수열의 변화

--- bronze2/no1551.py
+++ bronze2/no1551.py
@@ -1,16 +1,6 @@
-from win32con import EXCEPTION_ARRAY_BOUNDS_EXCEEDED
-
-
-def subtract(a, b):
-    x = a - b
-    return x
-
-
-n, c = map(int, input().split())
-A = list(map(int, input().split(",")))
-for i in range(len(A)):
-    if len(A) < A[i+1]:
-        break
-    A[i+1] - A[i]
-# print(subtract(3, -5))
-# print(subtract(A[0], A[1]))
+N, K = map(int, input().split())
+li = list(map(int, input().split(',')))
+for _ in range(K):
+    t = [li[i+1]-li[i] for i in range(len(li)-1)]
+    li = t
+print(*li, sep=',')
