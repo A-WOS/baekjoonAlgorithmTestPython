c7f55201afb72c128d4a6a7b1eda7b0ac4792144
Awos <uws0331@gmail.com>
2022-03-06 23:34:07 +0900

    백준 1934번 최소공배수 (함수명 변경)

--- silver5/no1934.py
+++ silver5/no1934.py
@@ -1,11 +1,11 @@
 from math import gcd
 
 
-def rs_gcd(x, y):
+def lcm(x, y):
     return x * y // gcd(x, y)
 
 
 t = int(input())
 for _ in range(t):
     A, B = map(int, input().split())
-    print(rs_gcd(A, B))
+    print(lcm(A, B))
