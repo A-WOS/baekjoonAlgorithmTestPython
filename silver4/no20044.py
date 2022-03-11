a472524458c01a236341bf7aac0f675c057c21f9
Awos <uws0331@gmail.com>
2022-03-11 23:33:50 +0900

    백준 20044번 Project Teams

---
+++ silver4/no20044.py
@@ -0,0 +1,26 @@
+import sys
+
+teams = int(input())
+students = sorted(map(int, sys.stdin.readline().split()))
+left, right = 0, teams*2 - 1
+rs = 199999
+while left < right:
+    if students[left] + students[right] < rs:
+        rs = students[left] + students[right]
+    left += 1
+    right -= 1
+print(rs)
+
+# false
+# students = set(map(int, sys.stdin.readline().split()))
+# # rs = set()
+# rs = 0
+# # for _ in range(teams):
+# for i in range(teams - 1):
+#     # rs.add(max(students)+min(students))
+#     # rs = max(students)+min(students)
+#     max(students[i]) + min(students[i])
+#     students.remove(max(students))
+#     students.remove(min(students))
+#
+# print(min(rs))
