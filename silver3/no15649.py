f50c969e34097f08569177f669f49308aa4d8085
Awos <uws0331@gmail.com>
2022-03-16 23:36:30 +0900

    백준 15649번 N과 M(1) (쓸데없는 코드 삭제)

--- silver3/no15649.py
+++ silver3/no15649.py
@@ -3,9 +3,5 @@ from itertools import permutations
 
 N, M = map(int, input().split())
 numbers = list(range(1, N + 1))
-if M == 1:
-    for val in numbers:
-        print(val)
-else:
-    for key in permutations(numbers, M):
-        print(re.sub(r'[^0-9\s]', '', str(key)))
+for key in permutations(numbers, M):
+    print(re.sub(r'[^0-9\s]', '', str(key)))
