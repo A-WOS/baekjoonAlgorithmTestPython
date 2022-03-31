352f250a5be2e0b5b3339174ae768abfa3d53fe8
Awos <uws0331@gmail.com>
2022-03-31 20:22:32 +0900

    백준 1620번 나는야 포켓몬 마스터 이다솜

---
+++ silver4/no1620.py
@@ -0,0 +1,16 @@
+import collections
+import sys
+
+N, rp = map(int, input().split())
+pocket_mons = collections.defaultdict(str)
+for i in range(N):
+    pocket_mons[i + 1] = sys.stdin.readline().rstrip()
+
+switch_pocket_mons = dict(zip(pocket_mons.values(), pocket_mons.keys()))
+for _ in range(rp):
+    rs = sys.stdin.readline().rstrip()
+    try:
+        if int(rs):
+            print(pocket_mons[int(rs)])
+    except:
+        print(switch_pocket_mons[rs])
