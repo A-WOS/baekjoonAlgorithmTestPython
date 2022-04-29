106ca1a464b017a8a979808348c91f4cf295c57c
AWOS <uws0331@gmail.com>
2022-04-29 23:08:57 +0900

    백준 1032번 명령 프롬프트

--- bronze1/no1032.py
+++ bronze1/no1032.py
@@ -1,7 +1,21 @@
+import sys
+
 n = int(input())
-file_name = set()
+file_name = []
+rs = ''
 for _ in range(n):
-    file_name.add(input())
-print(file_name)
-for val in file_name:
-    print(val)
+    file_name.append(list(sys.stdin.readline().rstrip()))
+
+# 문자열의 길이
+for i in range(len(file_name[0])):
+    # 같은 패턴인지 찾기 위해 set() 사용
+    diff_char = set()
+    # 입력받은 문자열 개수
+    for j in range(len(file_name)):
+        diff_char.add(file_name[j][i])
+
+    if len(diff_char) == 1:
+        rs += file_name[0][i]
+    else:
+        rs += '?'
+print(rs)
