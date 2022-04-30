6203d39eeacdceb1c8e11015fa2dfcc4521706c3
AWOS <uws0331@gmail.com>
2022-04-30 22:48:07 +0900

    백준 10798번 세로읽기

--- bronze1/no10798.py
+++ bronze1/no10798.py
@@ -1,8 +1,18 @@
-s = list()
-# while True:
-#     try:
-#         s.append(input())
-#     except EOFError:
-#         break
-for i in range(5):
-    s.append(input())
+import sys
+
+s = [list(sys.stdin.readline().rstrip()) for _ in range(5)]
+rs = ''
+max_len = 0
+for val in s:
+    if max_len < len(val): max_len = len(val)
+
+for val in s:
+    if len(val) != max_len:
+        for i in range(len(val), max_len):
+            val.append('')
+
+for i in range(len(s[0])):
+    for j in range(len(s)):
+        rs += s[j][i]
+
+print(rs)
